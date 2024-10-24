8865573d4f4c963978084773e34792b0c8ba187e
kajanan <thangarajahkajanan@gmail.com>
2024-10-24 19:52:41 +0530

    #4.3, created logout function

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+
+  const handleLogout = () => {
+    // clear the token from localStorage and redirect to login
+    localStorage.removeItem('token');
+    navigate('/');
+  };
+  
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
             <h1 className="text-3xl md:text-4xl text-center font-bold text-gray-700 mb-4">
                 Welcome to the Attendance System!
             </h1>
             <button
-                onClick={" "}
+                onClick={handleLogout}
                 className="px-12 py-3 mt-4 lg:mt-10 bg-teal-600 text-white font-bold rounded-lg hover:bg-teal-500"
             >
                 Logout
