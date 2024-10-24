cc7eecca8421ac5776daff8211f3c463b0200d72
kajanan <thangarajahkajanan@gmail.com>
2024-10-24 18:25:10 +0530

    #3.1, Created JwtUtil for generate token

---
+++ server/src/main/java/com/attendance/server/Util/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.attendance.server.Util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "my_secret_key";  // secure key
+    // Generating JWT token
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour expiration
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
